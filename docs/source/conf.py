25681492b5e8a29da6174f54218b51a0331b3a92
Brendt Wohlberg <brendt@ieee.org>
2021-09-21 12:09:30 -0600

    Manually run black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -317,12 +317,14 @@ if on_rtd:
     matplotlib.use("agg")
 
 
-MOCK_MODULES = ['astra', 'svmbir']
+MOCK_MODULES = ["astra", "svmbir"]
+
 
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
         return MagicMock()
+
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
