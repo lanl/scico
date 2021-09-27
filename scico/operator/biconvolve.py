3e0c68b20b37c3c8714b9166f3f2c02300ad40c1
Luke Pfister <luke.pfister@gmail.com>
2021-09-27 10:22:19 -0600

    Fix up biconvolve docstring

--- scico/operator/biconvolve.py
+++ scico/operator/biconvolve.py
@@ -1,14 +1,11 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2020-2021 by SCICO Developers
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SCICO package. Details of the copyright and
 # user license can be found in the 'LICENSE' file distributed with the
 # package.
 
-"""Bi."""
-
-
-# needed to annotate a class method that returns the encapsulating class
-# see https://www.python.org/dev/peps/pep-0563/
+"""Biconvolution operator."""
 
 
 import numpy as np
@@ -20,6 +17,8 @@ from scico.blockarray import BlockArray
 from scico.linop import Convolve, ConvolveByX
 from scico.typing import BlockShape, DType, JaxArray
 from scico.util import is_nested
+
+__author__ = """Luke Pfister <luke.pfister@gmail.com>"""
 
 
 class BiConvolve(Operator):
