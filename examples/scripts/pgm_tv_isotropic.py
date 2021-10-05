4b3b1da5bd7621c7c6e6a2ea25eec895ebb75d83
Brendt Wohlberg <brendt@ieee.org>
2021-10-05 17:11:50 -0600

    Clean up

--- examples/scripts/pgm_tv_isotropic.py
+++ examples/scripts/pgm_tv_isotropic.py
@@ -8,17 +8,13 @@ r"""
 Isotropic Total Variation (Accelerated PGM)
 ===========================================
 
-This example demonstrates the use of class [pgm.AcceleratedPGM](../_autosummary/scico.pgm.rst#scico.pgm.AcceleratedPGM) to solve isotropic total variation (TV) regularization.
-It solves the denoising problem
+This example demonstrates the use of class [pgm.AcceleratedPGM](../_autosummary/scico.pgm.rst#scico.pgm.AcceleratedPGM) to solve isotropic total variation (TV) regularization. It solves the denoising problem
 
   $$\mathrm{argmin}_{\mathbf{x}} \; (1/2) \| \mathbf{y} - \mathbf{x} \|^2 + \lambda R(\mathbf{x}) \;,$$
 
-where $R$ is the isotropic TV: the sum of the norms of the gradient vectors at each point in the image $\mathbf{x}$.
-The same reconstruction is performed with anisotropic TV regularization for comparison;
-the isotropic version shows fewer block-like artifacts.
+where $R$ is the isotropic TV: the sum of the norms of the gradient vectors at each point in the image $\mathbf{x}$. The same reconstruction is performed with anisotropic TV regularization for comparison; the isotropic version shows fewer block-like artifacts.
 
 The solution via PGM is based on :cite:`beck-2009-tv`. This follows a dual approach that constructs a dual for the constrained denoising problem (the constraint given by restricting the solution to the [0,1] range). The PGM solution minimizes the resulting dual. In this case, switching between the two regularizers corresponds to switching between two different projectors.
-
 """
 
 from typing import Callable, Optional, Union
@@ -38,7 +34,8 @@ Create a ground truth image.
 """
 N = 256  # Image size
 
-# these steps create a ground truth image by spatially filtering noise
+
+# These steps create a ground truth image by spatially filtering noise
 kernel_size = N // 5
 key = jax.random.PRNGKey(1)
 x_gt = jax.random.uniform(key, shape=(N + kernel_size - 1, N + kernel_size - 1))
@@ -51,6 +48,7 @@ x_gt = (x_gt > jnp.percentile(x_gt, 25)).astype(float) + (x_gt > jnp.percentile(
 )
 x_gt = x_gt / x_gt.max()
 
+
 """
 Add noise to create a noisy test image.
 """
@@ -61,6 +59,7 @@ n = sigma * jax.random.normal(subkey, shape=x_gt.shape)
 
 y = x_gt + n
 
+
 """
 Define finite difference operator and adjoint
 """
@@ -70,10 +69,12 @@ Define finite difference operator and adjoint
 C = linop.FiniteDifference(input_shape=x_gt.shape, append=0)
 A = C.adj
 
+
 """
-Define initial estimation: a zero array
+Define initial estimate: a zero array
 """
 x0 = jnp.zeros(C(y).shape)
+
 
 """
 Define the dual of the total variation denoising problem
@@ -99,8 +100,7 @@ class DualTVLoss(loss.Loss):
 
 
 """
-Denoise with isotropic total variation.
-Define projector for isotropic total variation.
+Denoise with isotropic total variation. Define projector for isotropic total variation.
 """
 # Evaluation of functional set to zero
 class IsoProjector(functional.Functional):
@@ -148,9 +148,9 @@ hist_iso = solver_iso.itstat_object.history(transpose=True)
 # project to constraint set
 x_iso = jnp.clip(y - f_iso.lmbda * f_iso.A(x), 0.0, 1.0)
 
+
 """
-Denoise with anisotropic total variation for comparison.
-Define projector for anisotropic total variation.
+Denoise with anisotropic total variation for comparison. Define projector for anisotropic total variation.
 """
 # Evaluation of functional set to zero
 class AnisoProjector(functional.Functional):
@@ -191,14 +191,16 @@ x = solver.solve()
 # project to constraint set
 x_aniso = jnp.clip(y - f.lmbda * f.A(x), 0.0, 1.0)
 
+
 """
 Compute the data fidelity.
 """
 df = hist_iso.Objective[-1]
-print(f"data fidelity for isotropic TV was {df:.2e}")
+print(f"Data fidelity for isotropic TV was {df:.2e}")
 hist = solver.itstat_object.history(transpose=True)
 df = hist.Objective[-1]
-print(f"data fidelity for anisotropic TV was {df:.2e}")
+print(f"Data fidelity for anisotropic TV was {df:.2e}")
+
 
 """
 Plot results.
@@ -211,12 +213,12 @@ plot.imview(x_iso, title="Isotropic TV denoising", fig=fig, ax=ax[1, 0], **plt_a
 plot.imview(x_aniso, title="Anisotropic TV denoising", fig=fig, ax=ax[1, 1], **plt_args)
 fig.subplots_adjust(left=0.1, right=0.99, top=0.95, bottom=0.05, wspace=0.2, hspace=0.01)
 fig.colorbar(
-    ax[0, 0].get_images()[0], ax=ax, location="right", shrink=0.9, pad=0.05, label="arbitrary units"
+    ax[0, 0].get_images()[0], ax=ax, location="right", shrink=0.9, pad=0.05, label="Arbitrary Units"
 )
-fig.suptitle("denoising comparison")
+fig.suptitle("Denoising comparison")
 fig.show()
 
-# zoomed version
+# Zoomed version
 fig, ax = plot.subplots(nrows=2, ncols=2, sharex=True, sharey=True, figsize=(11, 10))
 plot.imview(x_gt, title="Ground truth", fig=fig, ax=ax[0, 0], **plt_args)
 plot.imview(y, title="Noisy version", fig=fig, ax=ax[0, 1], **plt_args)
@@ -226,9 +228,9 @@ ax[0, 0].set_xlim(N // 4, N // 4 + N // 2)
 ax[0, 0].set_ylim(N // 4, N // 4 + N // 2)
 fig.subplots_adjust(left=0.1, right=0.99, top=0.95, bottom=0.05, wspace=0.2, hspace=0.01)
 fig.colorbar(
-    ax[0, 0].get_images()[0], ax=ax, location="right", shrink=0.9, pad=0.05, label="arbitrary units"
+    ax[0, 0].get_images()[0], ax=ax, location="right", shrink=0.9, pad=0.05, label="Arbitrary Units"
 )
-fig.suptitle("denoising comparison (zoomed)")
+fig.suptitle("Denoising comparison (zoomed)")
 fig.show()
 
 input("\nWaiting for input to close figures and exit")
