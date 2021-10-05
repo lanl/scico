3f027e3eb4d3d4d6ee8ab2b7e4feade3db08a619
Brendt Wohlberg <brendt@ieee.org>
2021-10-05 17:54:39 -0600

    Clean up

--- examples/scripts/admm_tv_isotropic.py
+++ examples/scripts/admm_tv_isotropic.py
@@ -8,18 +8,13 @@ r"""
 Isotropic Total Variation
 =========================
 
-This example demonstrates isotropic total variation (TV) regularization.
-It solves the denoising problem
+This example demonstrates isotropic total variation (TV) regularization. It solves the denoising problem
 
   $$\mathrm{argmin}_{\mathbf{x}} \; (1/2) \| \mathbf{y} - \mathbf{x} \|^2 + \lambda R(\mathbf{x}) \;,$$
 
-where $R$ is the isotropic TV: the sum of the norms of the gradient vectors at each point in the image $\mathbf{x}$.
-The same reconstruction is performed with anisotropic TV regularization for comparison;
-the isotropic version shows fewer block-like artifacts.
+where $R$ is the isotropic TV: the sum of the norms of the gradient vectors at each point in the image $\mathbf{x}$. The same reconstruction is performed with anisotropic TV regularization for comparison; the isotropic version shows fewer block-like artifacts.
 
-In SCICO, switching between these two regularizers is a one-line change:
-replacing an [L1Norm](../_autosummary/scico.functional.rst#scico.functional.L1Norm) with a [L21Norm](../_autosummary/scico.functional.rst#scico.functional.L21Norm).
-
+In SCICO, switching between these two regularizers is a one-line change: replacing an [L1Norm](../_autosummary/scico.functional.rst#scico.functional.L1Norm) with a [L21Norm](../_autosummary/scico.functional.rst#scico.functional.L21Norm).
 """
 
 import jax
@@ -34,7 +29,7 @@ Create a ground truth image.
 """
 N = 256  # Image size
 
-# these steps create a ground truth image by spatially filtering noise
+# These steps create a ground truth image by spatially filtering noise
 kernel_size = N // 5
 key = jax.random.PRNGKey(1)
 x_gt = jax.random.uniform(key, shape=(N + kernel_size - 1, N + kernel_size - 1))
@@ -47,15 +42,17 @@ x_gt = (x_gt > jnp.percentile(x_gt, 25)).astype(float) + (x_gt > jnp.percentile(
 )
 x_gt = x_gt / x_gt.max()
 
+
 """
 Add noise to create a noisy test image.
 """
-sigma = 1.0  # noise standard deviation
+sigma = 1.0  # Noise standard deviation
 key, subkey = jax.random.split(key)
 
 n = sigma * jax.random.normal(subkey, shape=x_gt.shape)
 
 y = x_gt + n
+
 
 """
 Denoise with isotropic total variation
@@ -64,7 +61,7 @@ reg_weight_iso = 2e0
 f = loss.SquaredL2Loss(y=y)
 g_iso = reg_weight_iso * functional.L21Norm()
 
-# the append=0 option makes the results of horizontal and vertical finite differences
+# The append=0 option makes the results of horizontal and vertical finite differences
 # the same shape, which is required for the L21Norm
 C = linop.FiniteDifference(input_shape=x_gt.shape, append=0)
 solver = ADMM(
@@ -82,10 +79,11 @@ solver.solve()
 
 x_iso = solver.x
 
+
 """
 Denoise with anisotropic total variation for comparison.
 """
-# we tune the weight to give the same data fidelty as the isotropic case
+# We tune the weight to give the same data fidelty as the isotropic case
 reg_weight_aniso = 1.74e0
 g_aniso = reg_weight_aniso * functional.L1Norm()
 
@@ -104,12 +102,14 @@ solver.solve()
 
 x_aniso = solver.x
 
+
 """
 Compute the data fidelity.
 """
-for x, name in zip((x_iso, x_aniso), ("isotropic", "anisotropic")):
+for x, name in zip((x_iso, x_aniso), ("Isotropic", "Anisotropic")):
     df = f(x)
-    print(f"data fidelity for {name} TV was {df:.2e}")
+    print(f"Data fidelity for {name} TV was {df:.2e}")
+
 
 """
 Plot results.
@@ -122,12 +122,12 @@ plot.imview(x_iso, title="Isotropic TV denoising", fig=fig, ax=ax[1, 0], **plt_a
 plot.imview(x_aniso, title="Anisotropic TV denoising", fig=fig, ax=ax[1, 1], **plt_args)
 fig.subplots_adjust(left=0.1, right=0.99, top=0.95, bottom=0.05, wspace=0.2, hspace=0.01)
 fig.colorbar(
-    ax[0, 0].get_images()[0], ax=ax, location="right", shrink=0.9, pad=0.05, label="arbitrary units"
+    ax[0, 0].get_images()[0], ax=ax, location="right", shrink=0.9, pad=0.05, label="Arbitrary Units"
 )
-fig.suptitle("denoising comparison")
+fig.suptitle("Denoising comparison")
 fig.show()
 
-# zoomed version
+# Zoomed version
 fig, ax = plot.subplots(nrows=2, ncols=2, sharex=True, sharey=True, figsize=(11, 10))
 plot.imview(x_gt, title="Ground truth", fig=fig, ax=ax[0, 0], **plt_args)
 plot.imview(y, title="Noisy version", fig=fig, ax=ax[0, 1], **plt_args)
@@ -137,9 +137,9 @@ ax[0, 0].set_xlim(N // 4, N // 4 + N // 2)
 ax[0, 0].set_ylim(N // 4, N // 4 + N // 2)
 fig.subplots_adjust(left=0.1, right=0.99, top=0.95, bottom=0.05, wspace=0.2, hspace=0.01)
 fig.colorbar(
-    ax[0, 0].get_images()[0], ax=ax, location="right", shrink=0.9, pad=0.05, label="arbitrary units"
+    ax[0, 0].get_images()[0], ax=ax, location="right", shrink=0.9, pad=0.05, label="Arbitrary Units"
 )
-fig.suptitle("denoising comparison (zoomed)")
+fig.suptitle("Denoising comparison (zoomed)")
 fig.show()
 
 input("\nWaiting for input to close figures and exit")
