9e345956f6c5fdf7d39cd4ff96f25b3146d838b3
Brendt Wohlberg <brendt@ieee.org>
2021-10-19 17:32:35 -0600

    Add comments

--- examples/makeindex.py
+++ examples/makeindex.py
@@ -10,10 +10,12 @@ with open(dst, "w") as dstfile:
     print(".. _example_notebooks:\n", file=dstfile)
     with open(src, "r") as srcfile:
         for line in srcfile:
+            # Detect lines containing script filenames
             m = re.match(r"(\s+)- ([^\s]+).py", line)
             if m:
                 print(m.group(1) + prfx + m.group(2), file=dstfile)
             else:
                 print(line, end="", file=dstfile)
+                # Add toctree statements after section headings
                 if line[0:3] == line[0] * 3 and line[0] in ["-", "="]:
                     print("\n.. toctree::\n   :maxdepth: 1", file=dstfile)
