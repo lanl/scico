896547d6034091b336e08f2abd335b93d1090b0d
Brendt Wohlberg <brendt@ieee.org>
2021-10-26 06:54:44 -0600

    Add additional header indicator character

--- examples/makeindex.py
+++ examples/makeindex.py
@@ -83,5 +83,5 @@ with open(dst, "w") as dstfile:
             else:
                 print(line, end="", file=dstfile)
                 # Add toctree statements after section headings
-                if line[0:3] == line[0] * 3 and line[0] in ["-", "="]:
+                if line[0:3] == line[0] * 3 and line[0] in ["=", "-", "^"]:
                     print("\n.. toctree::\n   :maxdepth: 1", file=dstfile)
