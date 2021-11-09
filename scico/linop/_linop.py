974b2b994a647d6422d9faaad3d9cd1e59362f67
Brendt Wohlberg <brendt@ieee.org>
2021-11-09 07:17:25 -0700

    Add function to compute operator norm

--- scico/linop/_linop.py
+++ scico/linop/_linop.py
@@ -58,6 +58,40 @@ def power_iteration(A: LinearOperator, maxiter: int = 100, key: Optional[PRNGKey
     return mu, v
 
 
+def operator_norm(A: LinearOperator, maxiter: int = 100, key: Optional[PRNGKey] = None):
+    r"""Estimate the norm of a :class:`.LinearOperator`.
+
+    Estimate the operator norm
+    `induced <https://en.wikipedia.org/wiki/Matrix_norm#Matrix_norms_induced_by_vector_norms>`_
+    by the :math:`\ell_2` vector norm, i.e. for :class:`.LinearOperator`
+    :math:`A`,
+
+    .. math::
+
+       \| A \|_2 &= \max \{ \| A \mb{x} \|_2 \, : \, \| \mb{x} \|_2 \leq 1 \} \\
+                 &= \sqrt{ \lambda_{ \mathrm{max} }( A^H A ) }
+                 = \sigma_{\mathrm{max}}(A) \;,
+
+    where :math:`\lambda_{\mathrm{max}}(B)` and
+    :math:`\sigma_{\mathrm{max}}(B)` respectively denote the
+    largest eigenvalue of :math:`B` and the largest singular value of
+    :math:`B`. The value is estimated via power iteration, using
+    :func:`power_iteration`, to estimate
+    :math:`\lambda_{\mathrm{max}}(A^H A)`.
+
+    Args:
+        A: :class:`.LinearOperator` for which operator norm is desired.
+        maxiter: Maximum number of power iterations to use. Default: 100
+        key: Jax PRNG key. Defaults to None, in which case a new key is
+            created.
+
+    Returns:
+        float : Norm of operator :math:`A`.
+
+    """
+    return snp.sqrt(power_iteration(A.H @ A, maxiter, key)[0])
+
+
 def valid_adjoint(
     A: LinearOperator,
     AT: LinearOperator,
