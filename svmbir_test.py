955677ccd756d66ea7ba7e13d2bc8f3beeb978f0
Thilo Balke <thilo.balke@gmail.com>
2021-11-12 19:41:25 -0700

    add initial test script

---
+++ svmbir_test.py
@@ -0,0 +1,92 @@
+import matplotlib.pyplot as plt
+
+import scico
+import scico.numpy as snp
+from scico.linop import Diagonal, Identity
+from scico.linop.radon_svmbir import ParallelBeamProjector, SVMBIRWeightedSquaredL2Loss
+from scico.solver import cg
+
+# from scico.test.linop.test_linop import adjoint_AAt_test, adjoint_AtA_test
+
+
+def make_im(Nx, Ny, is_3d=True):
+    x, y = snp.meshgrid(snp.linspace(-1, 1, Nx), snp.linspace(-1, 1, Ny))
+
+    im = snp.where((x - 0.25) ** 2 / 3 + y ** 2 < 0.1, 1.0, 0.0)
+    if is_3d:
+        im = im[snp.newaxis, :, :]
+    im = im.astype(snp.float32)
+
+    return im
+
+
+def make_A(im, num_angles, num_channels):
+    angles = snp.linspace(0, snp.pi, num_angles, dtype=snp.float32)
+    A = ParallelBeamProjector(im.shape, angles, num_channels)
+
+    return A
+
+
+def cg_prox(f, v, λ):  # Question: Should this be part of some other module?
+    # prox:
+    #   arg min  1/2 || x - v ||^2 + λ α || A x - y ||^2_W
+    #      x
+    #
+    # solution at:
+    #   (I + λ 2α A^T W A) x = v + λ 2α A^T W y
+    W = f.W
+    A = f.A
+    scale = f.scale
+    y = f.y
+    hessian = f.hessian  # = (2α A^T W A)
+    lhs = Identity(v.shape) + λ * hessian
+    rhs = v + 2 * λ * scale * A.adj(W(y))
+    x, _ = cg(lhs, rhs, x0=v)
+    return x
+
+
+num_angles = 32
+Nx = 64
+Ny = Nx
+num_channels = Nx
+
+
+im = make_im(Nx, Ny, is_3d=False) / Nx * 10
+A = make_A(im, num_angles, num_channels)
+y = A @ im
+
+
+W = snp.exp(-y) * 20
+# W = snp.ones_like(y)
+λ = 0.01  # needs to be choses small enough so that solution is not unstable
+
+
+f = SVMBIRWeightedSquaredL2Loss(y=y, A=A, W=Diagonal(W))
+v, _ = scico.random.normal(im.shape, dtype=im.dtype)
+v *= im.max() * 0.5
+
+xprox_svmbir = f.prox(v, λ)
+
+
+xprox_cg = cg_prox(f, v, λ)
+
+
+print(snp.linalg.norm(xprox_svmbir - xprox_cg) / snp.linalg.norm(xprox_svmbir))
+
+
+fig, ax = plt.subplots(2, 2, figsize=[8, 8])
+
+hand = ax[0, 0].imshow(im, vmin=-0.5, vmax=0.5)
+ax[0, 0].set_title("im")
+plt.colorbar(hand, ax=ax[0, 0])
+hand = ax[0, 1].imshow(v, vmin=-0.5, vmax=0.5)
+ax[0, 1].set_title("v")
+plt.colorbar(hand, ax=ax[0, 1])
+hand = ax[1, 0].imshow(xprox_svmbir, vmin=-0.5, vmax=0.5)
+ax[1, 0].set_title("xprox_svmbir")
+plt.colorbar(hand, ax=ax[1, 0])
+hand = ax[1, 1].imshow(xprox_cg, vmin=-0.5, vmax=0.5)
+ax[1, 1].set_title("xprox_cg")
+plt.colorbar(hand, ax=ax[1, 1])
+
+plt.show()
