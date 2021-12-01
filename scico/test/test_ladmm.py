6160c58d827cc0392b5652514ded59e2f76af8e3
Brendt Wohlberg <brendt@ieee.org>
2021-11-30 22:41:13 -0700

    Improve test coverage

--- scico/test/test_ladmm.py
+++ scico/test/test_ladmm.py
@@ -1,8 +1,63 @@
 import numpy as np
 
+import jax
+
 import scico.numpy as snp
 from scico import functional, linop, loss, random
 from scico.ladmm import LinearizedADMM
+
+flag = False
+
+
+class TestMisc:
+    def setup_method(self, method):
+        np.random.seed(12345)
+        self.y = jax.device_put(np.random.randn(32, 33).astype(np.float32))
+        self.λ = 1e0
+
+    def test_ladmm(self):
+        maxiter = 2
+        μ = 1e-1
+        ν = 1e-1
+        A = linop.Identity(self.y.shape)
+        f = loss.SquaredL2Loss(y=self.y, A=A)
+        g = (self.λ / 2) * functional.BM3D()
+        C = linop.Identity(self.y.shape)
+
+        itstat_dict = {"Iter": "%d", "Time": "%8.2e"}
+
+        def itstat_func(obj):
+            return (obj.itnum, obj.timer.elapsed())
+
+        ladmm_ = LinearizedADMM(
+            f=f,
+            g=g,
+            C=C,
+            mu=μ,
+            nu=ν,
+            maxiter=maxiter,
+            verbose=False,
+        )
+        assert len(ladmm_.itstat_object.fieldname) == 4
+        assert snp.sum(ladmm_.x) == 0.0
+        ladmm_ = LinearizedADMM(
+            f=f,
+            g=g,
+            C=C,
+            mu=μ,
+            nu=ν,
+            maxiter=maxiter,
+            verbose=False,
+            itstat=(itstat_dict, itstat_func),
+        )
+        assert len(ladmm_.itstat_object.fieldname) == 2
+
+        def callback(obj):
+            global flag
+            flag = True
+
+        x = ladmm_.solve(callback=callback)
+        assert flag
 
 
 class TestReal:
@@ -17,7 +72,7 @@ class TestReal:
         λ = 1e0
         self.Amx = Amx
         self.Bmx = Bmx
-        self.y = y
+        self.y = jax.device_put(y)
         self.λ = λ
         # Solution of problem is given by linear system (A^T A + λ B^T B) x = A^T y
         self.grdA = lambda x: (Amx.T @ Amx + λ * Bmx.T @ Bmx) @ x
@@ -57,7 +112,7 @@ class TestComplex:
         λ = 1e0
         self.Amx = Amx
         self.Bmx = Bmx
-        self.y = y
+        self.y = jax.device_put(y)
         self.λ = λ
         # Solution of problem is given by linear system (A^T A + λ B^T B) x = A^T y
         self.grdA = lambda x: (Amx.conj().T @ Amx + λ * Bmx.conj().T @ Bmx) @ x
