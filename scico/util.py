b410db16131c4df335b051ac44bcc16db5154b1b
Brendt Wohlberg <brendt@ieee.org>
2022-01-08 13:44:44 -0700

    Add missing typing import

--- scico/util.py
+++ scico/util.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2020-2021 by SCICO Developers
+# Copyright (C) 2020-2022 by SCICO Developers
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SCICO package. Details of the copyright and
 # user license can be found in the 'LICENSE' file distributed with the
@@ -18,7 +18,7 @@ import urllib.request as urlrequest
 import warnings
 from functools import wraps
 from timeit import default_timer as timer
-from typing import Any, Callable, List, Optional, Union
+from typing import Any, Callable, List, Optional, Tuple, Union
 
 import numpy as np
 
