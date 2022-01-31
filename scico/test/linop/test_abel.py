d5f7b9c52bf460769ce5ffa2556916113c8114ab
Soumendu Majee <soumendu.majee.1@gmail.com>
2022-01-31 15:42:19 -0700

    update tests

--- scico/test/linop/test_abel.py
+++ scico/test/linop/test_abel.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+import jax
+
 import pytest
 
 import scico.numpy as snp
@@ -16,19 +20,19 @@ def make_im(Nx, Ny):
     return im
 
 
-# @pytest.mark.parametrize("Nx, Ny", (BIG_INPUT,))
-# def test_grad(Nx, Ny):
-#     im = make_im(Nx, Ny)
+@pytest.mark.parametrize("Nx, Ny", (BIG_INPUT,))
+def test_grad(Nx, Ny):
+    im = make_im(Nx, Ny)
 
-#     A = AbelProjector(im.shape)
+    A = AbelProjector(im.shape)
 
-#     def f(im):
-#         return snp.sum(A._eval(im) ** 2)
+    def f(im):
+        return snp.sum(A._eval(im) ** 2)
 
-#     val_1 = jax.grad(f)(im)
-#     val_2 = 2 * A.adj(A(im))
+    val_1 = jax.grad(f)(im)
+    val_2 = 2 * A.adj(A(im))
 
-#     np.testing.assert_allclose(val_1, val_2, rtol=1)
+    np.testing.assert_allclose(val_1, val_2, rtol=1)
 
 
 @pytest.mark.parametrize("Nx, Ny", (BIG_INPUT,))
@@ -37,3 +41,15 @@ def test_adjoint(Nx, Ny):
     A = AbelProjector(im.shape)
 
     adjoint_test(A)
+
+
+# @pytest.mark.parametrize("Nx, Ny", (BIG_INPUT,))
+# def test_adjoint_grad(Nx, Ny):
+#     x = make_im(Nx, Ny)
+#     A = AbelProjector(x.shape)
+#     Ax = A @ x
+
+#     f = lambda y: jax.numpy.linalg.norm(A.T(y)) ** 2
+#     print(scico.grad(f)(Ax))
+#     print(2 * A(A.adj(Ax)))
+#     np.testing.assert_allclose(scico.grad(f)(Ax), 2 * A(A.adj(Ax)), rtol=5e-5)
