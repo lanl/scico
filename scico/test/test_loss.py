bdc02597527cc54c22e7d497ce418f865924ef2d
Brendt Wohlberg <brendt@ieee.org>
2022-03-01 14:29:20 -0700

    Clean up tests

--- scico/test/test_loss.py
+++ scico/test/test_loss.py
@@ -12,6 +12,7 @@ from prox import prox_test
 
 import scico.numpy as snp
 from scico import functional, linop, loss
+from scico.array import complex_dtype
 from scico.random import randn
 
 
@@ -131,84 +132,86 @@ class TestLoss:
         assert cL.scale == self.scalar * L.scale
         assert cL(v) == self.scalar * L(v)
 
-    def test_weighted_squared_l2_abs(self):
-        L = loss.WeightedSquaredL2AbsLoss(y=self.y, A=self.Ao, W=self.W)
+
+class TestComplexLoss:
+
+    cplx_loss = (
+        (loss.WeightedSquaredL2AbsLoss, lambda x: snp.abs(x)),
+        (loss.WeightedSquaredL2AbsSquaredLoss, lambda x: snp.abs(x) ** 2),
+    )
+
+    def setup_method(self):
+        n = 4
+        dtype = np.float64
+        A, key = randn((n, n), key=None, dtype=dtype, seed=1234)
+        W, key = randn((n,), key=key, dtype=dtype)
+        W = 0.1 * W + 1.0
+        self.Ao = linop.MatrixOperator(A)
+        self.Ao_abs = linop.MatrixOperator(snp.abs(A))
+        self.W = linop.Diagonal(W)
+        self.x, key = randn((n,), key=key, dtype=complex_dtype(dtype))
+        self.v, key = randn((n,), key=key, dtype=complex_dtype(dtype))  # point for prox eval
+        scalar, key = randn((1,), key=key, dtype=dtype)
+        self.scalar = scalar.copy().ravel()[0]
+
+    @pytest.mark.parametrize("loss_tuple", cplx_loss)
+    def test_properties(self, loss_tuple):
+        loss_class = loss_tuple[0]
+        loss_func = loss_tuple[1]
+
+        y = loss_func(self.Ao(self.x))
+        L = loss_class(y=y, A=self.Ao, W=self.W)
         assert L.has_eval
         assert not L.has_prox
-
-        # test eval
-        np.testing.assert_allclose(
-            L(self.v), 0.5 * (self.W @ (snp.abs(self.Ao @ self.v) - self.y) ** 2).sum()
-        )
 
         cL = self.scalar * L
         assert L.scale == 0.5  # hasn't changed
         assert cL.scale == self.scalar * L.scale
         assert cL(self.v) == self.scalar * L(self.v)
 
-        # Loss has a prox with Identity linop
-        y = snp.abs(self.y)
-        L_d = loss.WeightedSquaredL2AbsLoss(y=y, A=None, W=self.W)
-
-        assert L_d.has_eval
-        assert L_d.has_prox
-
-        # test eval
-        np.testing.assert_allclose(L_d(self.v), 0.5 * (self.W @ (snp.abs(self.v) - y) ** 2).sum())
-
-        cL = self.scalar * L_d
-        assert L_d.scale == 0.5  # hasn't changed
-        assert cL.scale == self.scalar * L_d.scale
-        assert cL(self.v) == self.scalar * L_d(self.v)
-
-        pf = prox_test(self.v, L_d, L_d.prox, 0.5)  # real v
-        v, key = randn(y.shape, key=self.key, dtype=np.complex128)
-        pf = prox_test(v, L_d, L_d.prox, 2.0)  # complex v
-        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L_d, L_d.prox, 0.0)  # complex zero v
-        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L_d, L_d.prox, 1.0)  # complex zero v
-        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L_d, L_d.prox, 2.0)  # complex zero v
-
         with pytest.raises(NotImplementedError):
-            pf = prox_test(self.v, L, L.prox, 0.75)
+            px = L.prox(self.v, 0.75)
 
-    def test_weighted_squared_l2_abs_squared(self):
-        L = loss.WeightedSquaredL2AbsSquaredLoss(y=self.y, A=self.Ao, W=self.W)
+        np.testing.assert_allclose(L(self.x), 0)
+
+        y = loss_func(self.x)
+        L = loss_class(y=y, A=None, W=self.W)
         assert L.has_eval
-        assert not L.has_prox
-
-        # test eval
-        np.testing.assert_allclose(
-            L(self.v), 0.5 * (self.W @ (snp.abs(self.Ao @ self.v) ** 2 - self.y) ** 2).sum()
-        )
+        assert L.has_prox
 
         cL = self.scalar * L
         assert L.scale == 0.5  # hasn't changed
         assert cL.scale == self.scalar * L.scale
         assert cL(self.v) == self.scalar * L(self.v)
 
-        # Loss has a prox with Identity linop
-        y = snp.abs(self.y)
-        L_d = loss.WeightedSquaredL2AbsSquaredLoss(y=y, A=None, W=self.W)
+        np.testing.assert_allclose(L(self.x), 0)
 
-        assert L_d.has_eval
-        assert L_d.has_prox
+    @pytest.mark.parametrize("loss_tuple", cplx_loss)
+    def test_prox(self, loss_tuple):
+        loss_class = loss_tuple[0]
+        loss_func = loss_tuple[1]
 
-        # test eval
-        np.testing.assert_allclose(
-            L_d(self.v), 0.5 * (self.W @ (snp.abs(self.v) ** 2 - y) ** 2).sum()
-        )
+        y = loss_func(self.x)
+        L = loss_class(y=y, A=None, W=self.W)
 
-        cL = self.scalar * L_d
-        assert L_d.scale == 0.5  # hasn't changed
-        assert cL.scale == self.scalar * L_d.scale
-        assert cL(self.v) == self.scalar * L_d(self.v)
+        pf = prox_test(self.v.real, L, L.prox, 0.5)  # real v
 
-        pf = prox_test(self.v, L_d, L_d.prox, 0.5)  # real v
-        v, key = randn(y.shape, key=self.key, dtype=np.complex128)
-        pf = prox_test(v, L_d, L_d.prox, 2.0)  # complex v
-        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L_d, L_d.prox, 0.0)  # complex zero v
-        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L_d, L_d.prox, 1.0)  # complex zero v
-        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L_d, L_d.prox, 2.0)  # complex zero v
+        pf = prox_test(self.v, L, L.prox, 0.0)  # complex v
+        pf = prox_test(self.v, L, L.prox, 0.1)  # complex v
+        pf = prox_test(self.v, L, L.prox, 2.0)  # complex v
 
-        with pytest.raises(NotImplementedError):
-            pf = prox_test(self.v, L, L.prox, 0.75)
+        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L, L.prox, 0.0)  # complex zero v
+        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L, L.prox, 1.0)  # complex zero v
+        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L, L.prox, 2.0)  # complex zero v
+
+        # zero y
+        y = snp.zeros(self.x.shape)
+        L = loss_class(y=y, A=None, W=self.W)
+
+        pf = prox_test(self.v.real, L, L.prox, 0.5)  # real v
+
+        pf = prox_test(self.v, L, L.prox, 0.0)  # complex v
+        pf = prox_test(self.v, L, L.prox, 0.1)  # complex v
+
+        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L, L.prox, 0.0)  # complex zero v
+        pf = prox_test((1 + 1j) * snp.zeros(self.v.shape), L, L.prox, 1.0)  # complex zero v
