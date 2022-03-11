31152ce33d8807d607a1f1b8d387a157e7a65f36
Michael-T-McCann <michael.thompson.mccann@gmail.com>
2022-03-11 10:40:11 -0700

    Fix path separators

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -162,7 +162,7 @@ copyright = "2020-2022, SCICO Developers"
 # built documents.
 #
 # The short X.Y version.
-with open(os.path.join(confpath, "../../scico", "__init__.py")) as f:
+with open(os.path.join(confpath, "..", "..", "scico", "__init__.py")) as f:
     version = parse(next(filter(lambda line: line.startswith("__version__"), f))).body[0].value.s
 # The full version, including alpha/beta/rc tags.
 release = version
