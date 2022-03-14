879f25ce5654613b2f701a85ab5b92caf3639f24
Soumendu Majee <soumendu.majee.1@gmail.com>
2022-03-14 17:05:21 -0600

    fix docs

--- scico/linop/radon_svmbir.py
+++ scico/linop/radon_svmbir.py
@@ -41,6 +41,8 @@ class TomographicProjector(LinearOperator):
     :meth:`.SVMBIRSquaredL2Loss.prox` when :class:`.SVMBIRSquaredL2Loss`
     is initialized with a :class:`TomographicProjector` with this option
     enabled.
+    Details about the underlying scanner geometries can be found
+    `here <https://svmbir.readthedocs.io/en/latest/overview.html>`.
     """
 
     def __init__(
@@ -56,10 +58,17 @@ class TomographicProjector(LinearOperator):
     ):
         """
         Args:
-            input_shape: Shape of the input array.
+            input_shape: Shape of the input array. Can be of length
+                either 2 (2D array) or 3 (3D array). When specifying
+                a 2D array the format for the input_shape is
+                (num_rows,num_cols). For a 3D array, the format for
+                the input_shape is (num_slices,num_rows,num_cols).
+                Note that (num_rows,num_cols) and (1,num_rows,num_cols)
+                result in the same underlying projector.
             angles: Array of projection angles in radians, should be
                 increasing.
-            num_channels: Number of pixels in the sinogram.
+            num_channels: Number of detector channels in the sinogram
+                data.
             center_offset: Position of the detector center relative to
                 the center of rotation, in units of pixels.
             is_masked: If ``True``, the valid region of the image is
@@ -69,9 +78,11 @@ class TomographicProjector(LinearOperator):
             geometry:  Scanner geometry, either "parallel" or "fan".
                 Note for fan geometry the  `dist_source_detector` and
                 `magnification` arguments must be specified.
-
-            dist_source_detector:
-            magnification:
+            dist_source_detector:  Distance from X-ray focal spot to
+                detectors in units of pixel pitch. Only used when geometry
+                is "fan".
+            magnification: Magnification factor of the scanner geometry.
+                Only used when geometry is "fan".
         """
         self.angles = angles
         self.num_channels = num_channels
