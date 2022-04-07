70a4551088148c296a5e07bd53c8a0725d259ad8
crstngc <cristina.cgarcia@gmail.com>
2022-04-07 15:18:52 -0600

    Fix examples flax test to skip foam test if xdesign not installed

--- scico/test/test_examples_flax.py
+++ scico/test/test_examples_flax.py
@@ -20,7 +20,15 @@ os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
 
 # These tests are for the scico.examples_flax module, NOT the example scripts
 
+try:
+    pass
+except ImportError:
+    have_xdesign = False
+else:
+    have_xdesign = True
 
+
+@pytest.mark.skipif(not have_xdesign, reason="xdesign package not installed")
 def test_foam2_gen():
     seed = 4321
     N = 32
@@ -30,10 +38,15 @@ def test_foam2_gen():
     assert dt.shape == (ndata, N, N, 1)
 
 
+def fake_data_gen(seed, N, ndata):
+    dt, key = random.randn((ndata, N, N, 1), seed=seed)
+    return dt
+
+
 def test_distdatagen():
     N = 32
     nimg = 16
-    dt = distributed_data_generation(generate_foam2_images, N, nimg)
+    dt = distributed_data_generation(fake_data_gen, N, nimg)
     assert dt.ndim == 5
     assert dt.shape[0] * dt.shape[1] == nimg
     assert dt.shape[2:] == (N, N, 1)
@@ -42,7 +55,7 @@ def test_distdatagen():
 def test_distdatagen_flag():
     N = 32
     nimg = 16
-    dt = distributed_data_generation(generate_foam2_images, N, nimg, False)
+    dt = distributed_data_generation(fake_data_gen, N, nimg, False)
     assert dt.ndim == 4
     assert dt.shape == (nimg, N, N, 1)
 
@@ -51,7 +64,7 @@ def test_distdatagen_exception():
     N = 32
     nimg = 15
     with pytest.raises(ValueError):
-        distributed_data_generation(generate_foam2_images, N, nimg)
+        distributed_data_generation(fake_data_gen, N, nimg)
 
 
 def test_rotation90():
