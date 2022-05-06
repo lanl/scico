4dc86b8e60d03b45176dbe51d5dd059a233a924b
Brendt Wohlberg <bwohlberg@users.noreply.github.com>
2022-05-06 18:37:39 +0200

    Restore missing `BlockArray` docs (#285)
    * Trivial change
    
    * Make consistent with docs_requirements.txt
    
    * Resolve missing blockarray docs
    
    * Add label
    
    * Docs improvements
    
    * Minor change for consistency
    
    * Fix typo
    
    * Fix docs
    
    * Hack to ignore dtype in doctest
    
    Co-authored-by: Michael-T-McCann <michael.thompson.mccann@gmail.com>

--- scico/numpy/util.py
+++ scico/numpy/util.py
@@ -1,4 +1,4 @@
-""" Utility functions for working with BlockArrays and DeviceArrays. """
+"""Utility functions for working with BlockArrays and DeviceArrays."""
 
 from __future__ import annotations
 
@@ -15,7 +15,7 @@ from jax.interpreters.xla import DeviceArray
 import scico.numpy as snp
 from scico.typing import ArrayIndex, Axes, AxisIndex, BlockShape, DType, JaxArray, Shape
 
-from .blockarray import BlockArray
+from ._blockarray import BlockArray
 
 
 def ensure_on_device(
@@ -34,7 +34,7 @@ def ensure_on_device(
 
     Returns:
         Modified array or arrays. Modified are only those that were
-           necessary.
+        necessary.
 
     Raises:
         TypeError: If the arrays contain something that is neither
