ec5b7ee3119c9b4906c9f426f45a42f24c97dae0
Brendt Wohlberg <brendt@ieee.org>
2022-05-09 21:38:45 -0600

    Improve example checking script

--- examples/scriptcheck.sh
+++ examples/scriptcheck.sh
@@ -1,8 +1,37 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 # Basic test of example script functionality by running them all with
 # optimization algorithms configured to use only a small number of iterations.
 # Currently only supported under Linux.
+
+SCRIPT=$(basename $0)
+SCRIPTPATH=$(realpath $(dirname $0))
+USAGE=$(cat <<-EOF
+Usage: $SCRIPT [-h] [-d]
+          [-h] Display usage information
+          [-d] Skip tests involving additional data downloads
+EOF
+)
+
+OPTIND=1
+SKIP_DOWNLOAD=0
+while getopts ":hd" opt; do
+    case $opt in
+	h) echo "$USAGE"; exit 0;;
+	d) SKIP_DOWNLOAD=1;;
+	\?) echo "Error: invalid option -$OPTARG" >&2
+            echo "$USAGE" >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+if [ ! $# -eq 0 ] ; then
+    echo "Error: no positional arguments" >&2
+    echo "$USAGE" >&2
+    exit 2
+fi
 
 # Check for presence of Xvfb tool which is used to avoid plots being displayed.
 if [ ! "$(which Xvfb 2>/dev/null)" ]; then
@@ -17,7 +46,7 @@ fi
 
 # Set environment variables and paths. This script is assumed to be run
 # from its root directory.
-export PYTHONPATH=$(cd .. && pwd)
+export PYTHONPATH=$SCRIPTPATH/..
 export PYTHONIOENCODING=utf-8
 d='/tmp/scriptcheck_'$$
 mkdir -p $d
@@ -36,16 +65,17 @@ re1="s/'maxiter' ?: ?[0-9]+/'maxiter': 2/g; "
 re2="s/^maxiter ?= ?[0-9]+/maxiter = 2/g; "
 re3="s/^N ?= ?[0-9]+/N = 32/g; "
 re4="s/num_samples= ?[0-9]+/num_samples = 4/g; "
-re5="s/input\(/#input\(/g; "
-re6="s/fig.show\(/#fig.show\(/g"
+re5="s/^downsampling_rate ?= ?[0-9]+/downsampling_rate = 12/g; "
+re6="s/input\(/#input\(/g; "
+re7="s/fig.show\(/#fig.show\(/g"
 
 # Iterate over all scripts.
-for f in scripts/*.py; do
+for f in $SCRIPTPATH/scripts/*.py; do
 
-    printf "%-50s " $f
+    printf "%-50s " $(basename $f)
 
     # Skip problem cases.
-    if grep -q '_microscopy' <<< $f; then
+    if [ $SKIP_DOWNLOAD -eq 1 ] && grep -q '_microscopy' <<< $f; then
 	printf "%s\n" skipped
 	continue
     fi
@@ -53,7 +83,7 @@ for f in scripts/*.py; do
     # Create temporary copy of script with all algorithm maxiter values set
     # to small number and final input statements commented out.
     g=$d/$(basename $f)
-    sed -E -e "$re1$re2$re3$re4$re5$re6" $f > $g
+    sed -E -e "$re1$re2$re3$re4$re5$re6$re7" $f > $g
 
     # Run temporary script and print status message.
     if python $g > /dev/null 2>&1; then
