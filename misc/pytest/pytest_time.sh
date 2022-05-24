d9b336928f64c25667ee775bf822d68cd59d1a96
Brendt Wohlberg <brendt@ieee.org>
2022-05-24 15:16:54 -0600

    Improve script

--- misc/pytest/pytest_time.sh
+++ misc/pytest/pytest_time.sh
@@ -3,11 +3,20 @@
 # This script runs each scico unit test module and lists them all in order
 # of decreasing run time. It must be run from the repository root directory.
 
+#if [ $(uname -s) != "Linux" ]; then
+#    echo "This script is only supported under Linux OS"
+#    exit 1
+#fi
+
 tmp=/tmp/pytest_time.$$
 rm -f $tmp
-for f in $(find scico/test/ -name "test_*.py"); do
-    t=$(/usr/bin/time -f "%e" pytest -qqq --disable-warnings $f 2>&1\
-	    | grep -o -E "[0-9\.]*$")
+for f in $(find scico/test -name "test_*.py"); do
+    tstr=$(/usr/bin/time -p pytest -qqq --disable-warnings $f 2>&1 | tail -4)
+    # Warning does not work in OSX bash
+    if grep -q "Command exited with non-zero status" <<<"$tstr"; then
+	echo "WARNING: test failure in $f" >&2
+    fi
+    t=$(grep "^real" <<<"$tstr" | grep -o -E "[0-9\.]*$")
     printf "%6.2f  %s\n" $t $f >> $tmp
 done
 sort -r -n $tmp
