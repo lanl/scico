690d2f65ae78e32bb45b4f290c625753825f3f80
Brendt Wohlberg <bwohlberg@users.noreply.github.com>
2022-05-31 15:54:15 -0600

    Add google colab badge (#308)
    * Add google colab badge
    
    * Address ipython/traitlets#741
    
    * Bump required sphinx version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,7 +69,7 @@ rootpath = os.path.abspath("../..")
 sys.path.insert(0, rootpath)
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = "3.5.2"
+needs_sphinx = "4.2.0"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
