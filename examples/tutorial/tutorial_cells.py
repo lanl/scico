21027c3d24647b25824c3293027f77615f4132c4
Michael-T-McCann <michael.thompson.mccann@gmail.com>
2022-08-12 13:05:46 -0700

    Update notebook

--- examples/tutorial/tutorial_cells.py
+++ examples/tutorial/tutorial_cells.py
@@ -7,6 +7,7 @@ You set up a new microscope in your lab and take a brightfield image, which we'l
 
 Run the next cell to see $y_1$.
 """
+%cd /content/scico-data/notebooks/tutorial  # set path
 
 import matplotlib.pyplot as plt
 
