bb7cb6c598f00867ab6e06e2edaaf46a51a0137e
Michael-T-McCann <michael.thompson.mccann@gmail.com>
2022-08-30 14:58:10 -0600

    Add pip line

--- examples/tutorial/tutorial_CNN.py
+++ examples/tutorial/tutorial_CNN.py
@@ -15,8 +15,8 @@ The install may take several minutes;
 when it is finished, you should see `==done with install==`.
 
 """
+!pip install -q condacolab
 import condacolab
-
 condacolab.install()
 
 !pip install git+https://github.com/lanl/scico@cristina/more-flax
