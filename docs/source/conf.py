96564c1ec34f8fe65b5bc803c62f7ff63fe43a1a
Brendt Wohlberg <brendt@ieee.org>
2022-09-21 18:20:41 -0600

    Avoid duplicate label warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -404,7 +404,7 @@ autoclass_content = "both"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "**tests**", "**spi**", "**README.rst", "**exampledepend.rst"]
+exclude_patterns = ["_build", "**tests**", "**spi**", "**README.rst", "include"]
 
 
 # Rewrite module names for certain functions imported into scico.numpy so that they are
