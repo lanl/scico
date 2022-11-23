1cd191c0435e896ad81bfea9f902bc124a1ab2d8
crstngc <cristina.cgarcia@gmail.com>
2022-11-23 14:03:46 -0700

    Update tests

--- scico/test/flax/test_examples_flax.py
+++ scico/test/flax/test_examples_flax.py
@@ -131,6 +131,27 @@ def test_ct_data_generation():
         assert fbp.shape == (nimg, N, N, 1)
 
 
+@pytest.mark.skipif(not have_astra, reason="astra package not installed")
+def test_ct_data_generation_jax():
+    N = 32
+    nimg = 8
+    nproj = 45
+
+    def random_img_gen(seed, size, ndata):
+        np.random.seed(seed)
+        return np.random.randn(ndata, size, size, 1)
+
+    try:
+        img, sino, fbp = generate_ct_data(nimg, N, nproj, imgfunc=random_img_gen, prefer_ray=False)
+    except Exception as e:
+        print(e)
+        assert 0
+    else:
+        assert img.shape == (nimg, N, N, 1)
+        assert sino.shape == (nimg, nproj, N, 1)
+        assert fbp.shape == (nimg, N, N, 1)
+
+
 def test_blur_data_generation():
     N = 32
     nimg = 8
@@ -144,6 +165,28 @@ def test_blur_data_generation():
     try:
         img, blurn = generate_blur_data(
             nimg, N, blur_kernel, noise_sigma=0.01, imgfunc=random_img_gen, test_flag=True
+        )
+    except Exception as e:
+        print(e)
+        assert 0
+    else:
+        assert img.shape == (nimg, N, N, 1)
+        assert blurn.shape == (nimg, N, N, 1)
+
+
+def test_blur_data_generation_jax():
+    N = 32
+    nimg = 8
+    n = 3  # convolution kernel size
+    blur_kernel = np.ones((n, n)) / (n * n)
+
+    def random_img_gen(seed, size, ndata):
+        np.random.seed(seed)
+        return np.random.randn(ndata, size, size, 1)
+
+    try:
+        img, blurn = generate_blur_data(
+            nimg, N, blur_kernel, noise_sigma=0.01, imgfunc=random_img_gen, prefer_ray=False
         )
     except Exception as e:
         print(e)
