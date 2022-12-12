c1465fdf0da3cdfc7a9d00aaf62f934f8e176187
Brendt Wohlberg <brendt@ieee.org>
2022-12-12 10:40:08 -0700

    Docstring changes

--- scico/denoiser.py
+++ scico/denoiser.py
@@ -53,8 +53,8 @@ def bm3d(x: JaxArray, sigma: float, is_rgb: bool = False, profile: Union[BM3DPro
         x: Input image. Expected to be a 2D array (gray-scale denoising)
             or 3D array (color denoising). Higher-dimensional arrays are
             tolerated only if the additional dimensions are singletons.
-            For color denoising, the color channel is assumed to be in the
-            last non-singleton dimension.
+            For color denoising, the color channel is assumed to be in
+            the last non-singleton dimension.
         sigma: Noise parameter.
         is_rgb: Flag indicating use of BM3D with a color transform.
             Default: ``False``.
@@ -177,35 +177,40 @@ def bm4d(x: JaxArray, sigma: float, profile: Union[BM4DProfile, str] = "np"):
 
 class DnCNN(FlaxMap):
     """Flax implementation of the DnCNN denoiser.
+
     A flax implementation of the DnCNN denoiser :cite:`zhang-2017-dncnn`.
     Note that :class:`.DnCNNNet` represents an untrained form of the
     generic DnCNN CNN structure, while this class represents a trained
     form with six or seventeen layers.
-    The standard DnCNN as proposed in :cite:`zhang-2017-dncnn` is a non-blind
-    denoiser without a noise level input. This implementation also supports
-    a custom variant of DnCNN that includes a noise level input (inspired by
-    :cite:`zhang-2021-plug`, which uses a different network architecture
-    known as DRUNET). This variant takes sigma (noise level) ranging from
-    0 to 0.2 as an extra input, concatenates the noisy image and an array
-    of sigma as a two-channel input of DnCNN, then generate a denoised
-    image corresponding to denoising strength controlled by the value
-    of sigma.
+
+    The standard DnCNN as proposed in :cite:`zhang-2017-dncnn` does not
+    have a noise level input. This implementation of DnCNN also supports
+    a custom variant that includes a noise standard deviation input,
+    `sigma`, which is included in the network as an additional channel
+    consisting of a constant array with value `sigma`. This network was
+    trained with image data on the range [0, 1], and with noise standard
+    deviations ranging from 0.0 to 0.2. It is worth noting that DRUNet
+    :cite:`zhang-2021-plug`, another recent approach to including a noise
+    level input in a CNN denoiser, is based on a substantially different
+    network architecture.
     """
 
     def __init__(self, variant: str = "6M"):
         """
         Note that all DnCNN models are trained for single-channel image
         input. Multi-channel input is supported via independent denoising
-        of each channel.
+        of each channel. Input images are expected to have pixel values
+        in the range [0, 1].
+
         Args:
             variant: Identify the DnCNN model to be used. Options are
                 '6L', '6M' (default), '6H', '6N', '17L', '17M', '17H',
                 and '17N', where the integer indicates the number of
                 layers in the network, and the postfix indicates the
                 training noise standard deviation (with respect to data
-                in the range [0, 1]): L (low) = 0.06, M (mid) = 0.1,
-                H (high) = 0.2, or N indicating that a noise level input
-                is available.
+                in the range [0, 1]): L (low) = 0.06, M (mid) = 0.10,
+                H (high) = 0.20, or N indicating that a noise standard
+                deviation input, `sigma`, is available.
         """
 
         self.variant = variant
@@ -225,6 +230,7 @@ class DnCNN(FlaxMap):
 
     def __call__(self, x: JaxArray, sigma: Optional[float] = None) -> JaxArray:
         r"""Apply DnCNN denoiser.
+
         Args:
             x: Input array.
             sigma: Noise standard deviation (for variants `6N` and `17N`).
