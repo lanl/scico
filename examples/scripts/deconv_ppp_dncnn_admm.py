70df967870aa9123203c5bfd87fcf8b40f5be005
Brendt Wohlberg <brendt@ieee.org>
2022-12-16 18:17:14 -0700

    Make related examples more consistent

--- examples/scripts/deconv_ppp_dncnn_admm.py
+++ examples/scripts/deconv_ppp_dncnn_admm.py
@@ -5,8 +5,8 @@
 # with the package.
 
 """
-PPP (with DnCNN) Image Deconvolution
-====================================
+PPP (with DnCNN) Image Deconvolution (ADMM Solver)
+==================================================
 
 This example demonstrates the solution of an image deconvolution problem
 using the ADMM Plug-and-Play Priors (PPP) algorithm
@@ -21,8 +21,7 @@ import jax
 from xdesign import Foam, discrete_phantom
 
 import scico.numpy as snp
-import scico.random
-from scico import functional, linop, loss, metric, plot
+from scico import functional, linop, loss, metric, plot, random
 from scico.optimize.admm import ADMM, LinearSubproblemSolver
 from scico.util import device_info
 
@@ -36,8 +35,8 @@ x_gt = jax.device_put(x_gt)  # convert to jax array, push to GPU
 
 
 """
-Set up forward operator and test signal consisting of blurred signal
-with additive Gaussian noise.
+Set up forward operator and test signal consisting of blurred signal with
+additive Gaussian noise.
 """
 n = 5  # convolution kernel size
 σ = 20.0 / 255  # noise level
@@ -46,24 +45,24 @@ psf = snp.ones((n, n)) / (n * n)
 A = linop.Convolve(h=psf, input_shape=x_gt.shape)
 
 Ax = A(x_gt)  # blurred image
-noise, key = scico.random.randn(Ax.shape, seed=0)
+noise, key = random.randn(Ax.shape)
 y = Ax + σ * noise
 
 
 """
-Load DnCNN denoiser and create map object.
+Create DnCNN denoiser presudo-functional.
 """
 g = functional.DnCNN("17M")
 
 
 """
-Set up an ADMM solver.
+Set up ADMM solver.
 """
-ρ = 0.2  # ADMM penalty parameter
-maxiter = 10  # number of ADMM iterations
-
 f = loss.SquaredL2Loss(y=y, A=A)
 C = linop.Identity(x_gt.shape)
+
+ρ = 0.2  # ADMM penalty parameter
+maxiter = 10  # number of ADMM iterations
 
 solver = ADMM(
     f=f,
@@ -91,9 +90,8 @@ Show the recovered image.
 """
 fig, ax = plot.subplots(nrows=1, ncols=3, figsize=(15, 5))
 plot.imview(x_gt, title="Ground truth", fig=fig, ax=ax[0])
-y = snp.clip(y, 0, 1)
 nc = n // 2
-yc = y[nc:-nc, nc:-nc]
+yc = snp.clip(y[nc:-nc, nc:-nc], 0, 1)
 plot.imview(y, title="Blurred, noisy image: %.2f (dB)" % metric.psnr(x_gt, yc), fig=fig, ax=ax[1])
 plot.imview(x, title="Deconvolved image: %.2f (dB)" % metric.psnr(x_gt, x), fig=fig, ax=ax[2])
 fig.show()
