ca3b5d1c37405e9550ccbcf69022f308b14b8efe
Brendt Wohlberg <brendt@ieee.org>
2023-03-04 14:06:23 -0700

    Minor docs edit

--- scico/functional/_indicator.py
+++ scico/functional/_indicator.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2020-2022 by SCICO Developers
+# Copyright (C) 2020-2023 by SCICO Developers
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SCICO package. Details of the copyright and
 # user license can be found in the 'LICENSE' file distributed with the
@@ -23,7 +23,7 @@ class NonNegativeIndicator(Functional):
     r"""Indicator function for non-negative orthant.
 
     Returns 0 if all elements of input array-like are non-negative, and
-    inf otherwise
+    `inf` otherwise
 
     .. math::
         I(\mb{x}) = \begin{cases}
