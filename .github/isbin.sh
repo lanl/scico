e3fcc5e3ae99d623dce8361b7ea5dd0a90044c9a
Brendt Wohlberg <bwohlberg@users.noreply.github.com>
2023-03-14 19:10:24 -0600

    Update favicon and fix pre-commit hook helper script (#384)
    * Improved favicon
    
    * Make script compatible with MacOS bash
    
    * Improvement recommended during PR review

--- .github/isbin.sh
+++ .github/isbin.sh
@@ -6,27 +6,32 @@ size_threshold=65536
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
+OS=$(uname -a | cut -d ' ' -f 1)
 
 for f in $@; do
     echo $f
-    size=$(stat --format %s $f)
+    case "$OS" in
+        Linux)  size=$(stat --format "%s" $f);;
+        Darwin) size=$(stat -f "%z" $f);;
+        *)      echo "Error: unsupported operating system $OS" >&2; exit 1;;
+    esac
     if [ $size -gt $size_threshold ]; then
-     echo "file exceeds maximum allowable size"
-     echo "raw data and ipynb files should go in scico-data"
-        exit 1
+        echo "file exceeds maximum allowable size of $size_threshold bytes"
+        echo "raw data and ipynb files should go in scico-data"
+        exit 2
     fi
     charset=$(file -b --mime $f | sed -e 's/.*charset=//')
     if [ ! -L "$f" ] && [ "$charset" = "binary" ]; then
-     echo "binary files cannot be commited to the repository"
-     echo "raw data and ipynb files should go in scico-data"
-        exit 2
+        echo "binary files cannot be commited to the repository"
+        echo "raw data and ipynb files should go in scico-data"
+        exit 3
     fi
     basename=$(basename -- "$f")
     ext="${basename##*.}"
     if [ "$ext" = "ipynb" ]; then
-     echo "ipynb files cannot be commited to the repository"
-     echo "raw data and ipynb files should go in scico-data"
-        exit 3
+        echo "ipynb files cannot be commited to the repository"
+        echo "raw data and ipynb files should go in scico-data"
+        exit 4
     fi
 done
 
