b3d4dc84c005ebe5ea327bed98bfbbb632844cc1
Brendt Wohlberg <brendt@ieee.org>
2023-05-09 16:56:20 -0600

    Avoid pollution of docs with excessively long type alias definition

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 from inspect import getmembers, isfunction
+from typing import Optional, Sequence, Union  # needed for typehints_formatter hack
 
 from sphinx.ext.napoleon.docstring import GoogleDocstring
 
@@ -15,6 +16,11 @@ sys.path.append(rootpath)
 from docutil import insert_inheritance_diagram, package_classes
 
 from scico._version import package_version
+from scico.typing import (  # needed for typehints_formatter hack
+    ArrayIndex,
+    AxisIndex,
+    DType,
+)
 
 
 ## See
@@ -338,11 +344,11 @@ if on_rtd:
 
     matplotlib.use("agg")
 
+print("Sphinx paths:")
+print(f"  rootpath: {rootpath}")
+print(f"  confpath: {confpath}")
 
-print("rootpath: %s" % rootpath)
-print("confpath: %s" % confpath)
 
-# Sort members by type
 autodoc_default_options = {
     "member-order": "bysource",
     "inherited-members": False,
@@ -353,6 +359,46 @@ autodoc_default_options = {
 }
 autodoc_docstring_signature = True
 autoclass_content = "both"
+
+
+# An explanation for this nasty hack, the primary purpose of which is to avoid
+# the very long definition of the scico.typing.DType appearing explicitly in the
+# docs. This is handled correctly by sphinx.ext.autodoc in some circumstances,
+# but only when sphinx_autodoc_typehints is not included in the extension list,
+# and the appearance of the type hints (e.g. whether links to definitions are
+# included) seems to depend on whether "from __future__ import annotations" was
+# used in the module being documented, which is not ideal from a consistency
+# perspective. (It's also worth noting that sphinx.ext.autodoc provides some
+# configurability for type aliases via the autodoc_type_aliases sphinx
+# configuration option.) The alternative is to include sphinx_autodoc_typehints,
+# which gives a consistent appearance to the type hints, but the
+# autodoc_type_aliases configuration option is ignored, and type aliases are
+# always expanded. This hack avoids expansion for the type aliases with the
+# longest definitions by definining a custom function for formatting the
+# type hints, using an option provided by sphinx_autodoc_typehints. For
+# more information, see
+#   https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_type_aliases
+#   https://github.com/tox-dev/sphinx-autodoc-typehints/issues/284
+#   https://github.com/tox-dev/sphinx-autodoc-typehints/blob/main/README.md
+def typehints_formatter_function(annotation, config):
+    markup = {
+        DType: ":obj:`~scico.typing.DType`",
+        Optional[DType]: ":obj:`~typing.Optional`\ [\ :obj:`~scico.typing.DType`\ ]",
+        Union[DType, Sequence[DType]]: (
+            ":obj:`~typing.Union`\ [\ :obj:`~scico.typing.DType`\ , "
+            ":obj:`~typing.Sequence`\ [\ :obj:`~scico.typing.DType`\ ]]"
+        ),
+        AxisIndex: ":obj:`~scico.typing.AxisIndex`",
+        ArrayIndex: ":obj:`~scico.typing.ArrayIndex`",
+    }
+    if annotation in markup:
+        return markup[annotation]
+    else:
+        return None
+
+
+typehints_formatter = typehints_formatter_function
+
 
 # See https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_mock_imports
 autodoc_mock_imports = ["astra", "svmbir", "ray"]
