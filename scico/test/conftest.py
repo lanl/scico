2bd8c135acdcfad956bb610370802fb2212ea5d8
Brendt Wohlberg <brendt@ieee.org>
2023-06-18 17:03:46 -0600

    Remove secondary conftest.py

--- scico/test/conftest.py
+++
@@ -1,8 +0,0 @@
-def pytest_addoption(parser):
-    # Level definitions:
-    #  1  Critical tests only
-    #  2  Skip tests that do have a significant impact on coverage
-    #  3  All tests
-    parser.addoption(
-        "--level", action="store", default=3, type=int, help="Set test level to be run"
-    )
