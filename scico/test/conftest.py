7a9f6c12b732e2ae203f47a9f982ca89fbf4cac7
Brendt Wohlberg <brendt@ieee.org>
2023-06-17 21:43:32 -0600

    Add note on level semantics and set levels in CI tests

--- scico/test/conftest.py
+++ scico/test/conftest.py
@@ -1,4 +1,8 @@
 def pytest_addoption(parser):
+    # Level definitions:
+    #  1  Critical tests only
+    #  2  Skip tests that do have a significant impact on coverage
+    #  3  All tests
     parser.addoption(
-        "--level", action="store", default=1, type=int, help="Set test level to be run"
+        "--level", action="store", default=3, type=int, help="Set test level to be run"
     )
