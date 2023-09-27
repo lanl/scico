83d129f58b082abef8165b59fac69864c2eb4709
Michael-T-McCann <michael.thompson.mccann@gmail.com>
2023-09-27 12:17:19 -0600

    Rename parameter

--- scico/test/linop/test_xray.py
+++ scico/test/linop/test_xray.py
@@ -22,5 +22,5 @@ def test_apply():
     assert y.shape[1] == det_length
 
     # dither off
-    H = XRayProject(ParallelFixedAxis2dProjector(x.shape, angles, do_dithering=False))
+    H = XRayProject(ParallelFixedAxis2dProjector(x.shape, angles, dither=False))
     y = H @ x
