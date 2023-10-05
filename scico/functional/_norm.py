c62da28ebc4e1de42452e0384800359977f5bfb2
Salman Naqvi <salman_naqvi@apple.com>
2023-10-05 16:43:33 -0700

    sort imports lint error

--- scico/functional/_norm.py
+++ scico/functional/_norm.py
@@ -12,10 +12,10 @@ from typing import Optional, Tuple, Union
 from jax import jit, lax
 
 from scico import numpy as snp
+from scico.linop import FiniteDifference
 from scico.numpy import Array, BlockArray, count_nonzero
 from scico.numpy.linalg import norm
 from scico.numpy.util import no_nan_divide
-from scico.linop import FiniteDifference
 
 from ._functional import Functional
 
