dc60d86ab57b3bbb3bd01ce2a9a242abd0f5cdb4
Brendt Wohlberg <brendt@ieee.org>
2023-10-06 05:28:08 -0600

    Docs edits

--- examples/scripts/ct_tv_admm.py
+++ examples/scripts/ct_tv_admm.py
@@ -5,8 +5,8 @@
 # with the package.
 
 r"""
-TV-Regularized Sparse-View CT Reconstruction
-============================================
+TV-Regularized Sparse-View CT Reconstruction (Integrated Projector)
+===================================================================
 
 This example demonstrates solution of a sparse-view CT reconstruction
 problem with isotropic total variation (TV) regularization
@@ -16,7 +16,10 @@ problem with isotropic total variation (TV) regularization
 
 where $A$ is the X-ray transform (the CT forward projection operator),
 $\mathbf{y}$ is the sinogram, $C$ is a 2D finite difference operator, and
-$\mathbf{x}$ is the desired image.
+$\mathbf{x}$ is the desired image. This example uses the CT projector
+integrated into scico, while the companion
+[example script](ct_astra_tv_admm.rst) uses the projector provided by
+the astra package.
 """
 
 import numpy as np
