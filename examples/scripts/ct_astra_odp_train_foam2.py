60263490c05dc5245ed2655455e42ef0d2b5d874
Cristina Garcia-Cardona <cgarciac@lanl.gov>
2023-11-14 15:33:19 -0700

    Add checkpointing flag to CT unet and odp examples

--- examples/scripts/ct_astra_odp_train_foam2.py
+++ examples/scripts/ct_astra_odp_train_foam2.py
@@ -133,6 +133,7 @@ train_conf: sflax.ConfigDict = {
     "warmup_epochs": 0,
     "log_every_steps": 160,
     "log": True,
+    "checkpointing": True,
 }
 
 
@@ -221,7 +222,9 @@ print(
     f"{'PSNR:':6s}{psnr_eval:>5.2f}{' dB'}{'':3s}{'time[s]:':10s}{time_eval:>7.2f}"
 )
 
-# Plot comparison
+"""
+Plot comparison.
+"""
 np.random.seed(123)
 indx = np.random.randint(0, high=maxn)
 
@@ -251,10 +254,10 @@ fig.show()
 
 
 """
-Plot convergence statistics. Statistics only generated if a training
-cycle was done (i.e. not reading final epoch results from checkpoint).
+Plot convergence statistics. Statistics are generated only if a training
+cycle was done (i.e. if not reading final epoch results from checkpoint).
 """
-if stats_object is not None:
+if stats_object is not None and len(stats_object.iterations) > 0:
     hist = stats_object.history(transpose=True)
     fig, ax = plot.subplots(nrows=1, ncols=2, figsize=(12, 5))
     plot.plot(
