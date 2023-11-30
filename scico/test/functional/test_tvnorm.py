5473e95b74d61064669f5e389a0ade9dfe92520c
Brendt Wohlberg <brendt@ieee.org>
2023-11-30 07:25:48 -0700

    Add 2d test

--- scico/test/functional/test_tvnorm.py
+++ scico/test/functional/test_tvnorm.py
@@ -1,13 +1,22 @@
 import numpy as np
 
+import pytest
+
+try:
+    from xdesign import SiemensStar, discrete_phantom
+
+    have_xdesign = True
+except ImportError:
+    have_xdesign = False
+
+import scico.numpy as snp
 import scico.random
 from scico import functional, linop, loss, metric
+from scico.optimize import AcceleratedPGM, ProximalADMM
 from scico.optimize.admm import ADMM, LinearSubproblemSolver
-from scico.optimize.pgm import AcceleratedPGM
 
 
-def test_aniso_tvnorm():
-
+def test_aniso_1d():
     N = 128
     g = np.linspace(0, 2 * np.pi, N, dtype=np.float32)
     x_gt = np.sin(2 * g)
@@ -38,3 +47,48 @@ def test_aniso_tvnorm():
     x_approx = solver.solve()
 
     assert metric.snr(x_tvdn, x_approx) > 45
+
+
+@pytest.mark.skipif(not have_xdesign, reason="xdesign package not installed")
+class Test2D:
+    def setup_method(self):
+        N = 128
+        σ = 0.25
+        phantom = SiemensStar(16)
+        x_gt = snp.pad(discrete_phantom(phantom, N - 16), 8)
+        x_gt = x_gt / x_gt.max()
+        noise, key = scico.random.randn(x_gt.shape, seed=0)
+        y = x_gt + σ * noise
+
+        self.x_gt = x_gt
+        self.y = y
+
+    def test_aniso(self):
+        x_gt = self.x_gt
+        y = self.y
+
+        λ = 2e-1
+        f = loss.SquaredL2Loss(y=y)
+        g = λ * functional.L1Norm()
+        C = linop.FiniteDifference(input_shape=x_gt.shape, circular=True)
+
+        mu, nu = ProximalADMM.estimate_parameters(C)
+        solver = ProximalADMM(
+            f=f,
+            g=g,
+            A=C,
+            rho=1e0,
+            mu=mu,
+            nu=nu,
+            x0=y,
+            maxiter=200,
+        )
+        x = solver.solve()
+
+        h = λ * functional.AnisotropicTVNorm()
+        solver = AcceleratedPGM(
+            f=f, g=h, L0=1e3, x0=y, maxiter=300, itstat_options={"display": True, "period": 20}
+        )
+        x_aprx = solver.solve()
+
+        assert metric.snr(x, x_aprx) > 30.0
