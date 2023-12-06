0852078d0019640a560e248edfd3d00d6cc073fe
Brendt Wohlberg <brendt@ieee.org>
2023-12-05 19:45:27 -0700

    Resolve some docs issues

--- scico/operator/__init__.py
+++ scico/operator/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2021-2022 by SCICO Developers
+# Copyright (C) 2021-2023 by SCICO Developers
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SCICO package. Details of the copyright and
 # user license can be found in the 'LICENSE' file distributed with the
@@ -13,7 +13,7 @@ import sys
 from ._operator import Operator
 from .biconvolve import BiConvolve
 from ._func import operator_from_function, Abs, Angle, Exp
-from ._stack import DiagonalStack, VerticalStack
+from ._stack import DiagonalStack, VerticalStack, AbstractDiagonalStack, AbstractVerticalStack
 
 __all__ = [
     "Operator",
@@ -26,6 +26,7 @@ __all__ = [
     "Exp",
 ]
 
-# Imported items in __all__ appear to originate in top-level linop module
-for name in __all__:
+# Imported items in __all__ and other appear to originate in top-level linop module
+other = ["AbstractDiagonalStack", "AbstractVerticalStack"]
+for name in __all__ + other:
     getattr(sys.modules[__name__], name).__module__ = __name__
