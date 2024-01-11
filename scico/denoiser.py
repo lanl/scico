5fba865445a97c9027ace5f2d2f085984cd1c5d9
Brendt Wohlberg <brendt@ieee.org>
2024-01-11 11:59:44 -0700

    Rename flax data files

--- scico/denoiser.py
+++ scico/denoiser.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2020-2023 by SCICO Developers
+# Copyright (C) 2020-2024 by SCICO Developers
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SCICO package. Details of the copyright and
 # user license can be found in the 'LICENSE' file distributed with the
@@ -234,7 +234,7 @@ class DnCNN(FlaxMap):
             self.is_blind = True
 
         model = DnCNNNet(depth=nlayer, channels=channels, num_filters=64, dtype=np.float32)
-        variables = load_weights(_flax_data_path("dncnn%s.npz" % variant))
+        variables = load_weights(_flax_data_path("dncnn%s.mpk" % variant))
         super().__init__(model, variables)
 
     def __call__(self, x: snp.Array, sigma: Optional[float] = None) -> snp.Array:
