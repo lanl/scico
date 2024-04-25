0f51c8baa89dd957263c8010e829e4666c0c022e
Brendt Wohlberg <brendt@ieee.org>
2024-04-25 17:02:16 -0600

    Rearrange ray initialization to avoid os.fork() RuntimeWarning

--- conftest.py
+++ conftest.py
@@ -1,21 +1,39 @@
 """
-Configure the environment in which doctests run. This is necessary
-because `np` is used in doc strings for jax functions
-(e.g. `linear_transpose`) that get pulled into `scico/__init__.py`.
-
-Also allow `snp` to be used without explicitly importing, and add
-`level` parameter.
+Configure pytest.
 """
 
 import numpy as np
 
 import pytest
 
+try:
+    import ray  # noqa: F401
+except ImportError:
+    have_ray = False
+else:
+    have_ray = True
+    ray.init(num_cpus=1)  # required to be here: see ray-project/ray#44087
+
 import scico.numpy as snp
+
+
+def pytest_sessionstart(session):
+    """Initialize before start of test session."""
+    # placeholder: currently unused
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """Clean up after end of test session."""
+    ray.shutdown()
 
 
 @pytest.fixture(autouse=True)
 def add_modules(doctest_namespace):
-    """Add common modules for use in docstring examples."""
+    """Add common modules for use in docstring examples.
+
+    Necessary because `np` is used in doc strings for jax functions
+    (e.g. `linear_transpose`) that get pulled into `scico/__init__.py`.
+    Also allow `snp` to be used without explicitly importing.
+    """
     doctest_namespace["np"] = np
     doctest_namespace["snp"] = snp
