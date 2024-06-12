013fd6f5756451e263997e66aaa815a694783e69
Brendt Wohlberg <brendt@ieee.org>
2024-06-12 10:38:41 -0600

    Add comment to test as per PR review

--- scico/test/linop/xray/test_astra.py
+++ scico/test/linop/xray/test_astra.py
@@ -124,6 +124,8 @@ def test_adjoint_typical_input(testobj):
 
 def test_fbp(testobj):
     x = testobj.A.fbp(testobj.y)
+    # Test for a bug (related to calling the Astra CPU FBP implementation
+    # when using a FPU device) that resulted in a constant zero output.
     assert np.sum(np.abs(x)) > 0.0
 
 
