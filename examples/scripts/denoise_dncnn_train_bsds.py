7e82aa1e7853be80abdb5d82b6e41ff08c047d50
Brendt Wohlberg <brendt@ieee.org>
2024-09-23 18:33:23 -0600

    Handle get_backend change in jax 0.4.33

--- examples/scripts/denoise_dncnn_train_bsds.py
+++ examples/scripts/denoise_dncnn_train_bsds.py
@@ -24,6 +24,11 @@ os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=8"
 
 import jax
 
+try:
+    from jax.extend.backend import get_backend  # introduced in jax 0.4.33
+except ImportError:
+    from jax.lib.xla_bridge import get_backend
+
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from scico import flax as sflax
@@ -31,7 +36,7 @@ from scico import metric, plot
 from scico.flax.examples import load_image_data
 
 
-platform = jax.lib.xla_bridge.get_backend().platform
+platform = get_backend().platform
 print("Platform: ", platform)
 
 
