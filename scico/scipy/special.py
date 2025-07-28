69926daa1b941a6c884cb8bdcc56589985751a52
Brendt Wohlberg <brendt@ieee.org>
2025-07-28 15:09:06 -0600

    Function sph_harm deprecated, replaced with sph_harm_y

--- scico/scipy/special.py
+++ scico/scipy/special.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2020-2022 by SCICO Developers
+# Copyright (C) 2020-2025 by SCICO Developers
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SCICO package. Details of the copyright and
 # user license can be found in the 'LICENSE' file distributed with the
@@ -44,7 +44,7 @@ functions = (
     "ndtr",
     "ndtri",
     "polygamma",
-    "sph_harm",
+    "sph_harm_y",
     "xlog1py",
     "xlogy",
     "zeta",
