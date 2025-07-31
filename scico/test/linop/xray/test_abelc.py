b62d1f7b05182e1d0e67d10c9f5f533396f30d57
Brendt Wohlberg <brendt@ieee.org>
2025-07-30 18:02:51 -0600

    Change config parameters

--- scico/test/linop/xray/test_abelc.py
+++ scico/test/linop/xray/test_abelc.py
@@ -52,32 +52,32 @@ class TestAbelCone:
 
     @pytest.mark.parametrize("num_blocks", [1, 2, 3])
     def test_2d(self, num_blocks):
-        A = AbelTransformCone(self.x2d.shape, (1.0, 1.0), 1e6, 1e6 + 1, num_blocks=num_blocks)
-        ya = A(self.x2d) / A.config.voxel_size_x
+        A = AbelTransformCone(self.x2d.shape, 1e6, 1e6 + 1, num_blocks=num_blocks)
+        ya = A(self.x2d)
         x2ds = _volume_by_axial_symmetry(self.x2d, axis=0)
         ys = np.sum(x2ds, axis=1)
-        assert metric.rel_res(ys, ya) < 1e-6
+        assert metric.rel_res(ys, ya) < 5e-6
 
     @pytest.mark.parametrize("num_blocks", [1, 2, 3])
     def test_2d_unequal(self, num_blocks):
         x2dc = self.x2d[1:-1]
-        A = AbelTransformCone(x2dc.shape, (1.0, 1.0), 1e6, 1e6 + 1, num_blocks=num_blocks)
-        ya = A(x2dc) / A.config.voxel_size_x
+        A = AbelTransformCone(x2dc.shape, 1e6, 1e6 + 1, num_blocks=num_blocks)
+        ya = A(x2dc)
         x2ds = _volume_by_axial_symmetry(x2dc, axis=0)
         ys = np.sum(x2ds, axis=1)
-        assert metric.rel_res(ys, ya) < 1e-6
+        assert metric.rel_res(ys, ya) < 5e-6
 
     @pytest.mark.parametrize("num_blocks", [1, 2, 3])
     def test_3d(self, num_blocks):
-        A = AbelTransformCone(self.x3d.shape, (1.0, 1.0), 1e6, 1e6 + 1, num_blocks=num_blocks)
-        ya = A(self.x3d) / A.config.voxel_size_x
+        A = AbelTransformCone(self.x3d.shape, 1e6, 1e6 + 1, num_blocks=num_blocks)
+        ya = A(self.x3d)
         ys = np.sum(self.x3d, axis=1)
-        assert metric.rel_res(ys, ya) < 1e-6
+        assert metric.rel_res(ys, ya) < 5e-6
 
     @pytest.mark.parametrize("num_blocks", [1, 2, 3])
     def test_3d_unequal(self, num_blocks):
         x3dc = self.x3d[1:-1, 2:-2]
-        A = AbelTransformCone(x3dc.shape, (1.0, 1.0), 1e6, 1e6 + 1, num_blocks=num_blocks)
-        ya = A(x3dc) / A.config.voxel_size_x
+        A = AbelTransformCone(x3dc.shape, 1e6, 1e6 + 1, num_blocks=num_blocks)
+        ya = A(x3dc)
         ys = np.sum(x3dc, axis=1)
-        assert metric.rel_res(ys, ya) < 1e-6
+        assert metric.rel_res(ys, ya) < 1e-5
