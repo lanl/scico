f6408cb3ecef62d2f0a94e1fc52cadf1fd48c4c9
crstngc <cristina.cgarcia@gmail.com>
2025-12-23 10:37:31 -0700

    Pop list of skip connections in unet to reduce memory

--- scico/flax_nnx/_models.py
+++ scico/flax_nnx/_models.py
@@ -12,10 +12,10 @@ import warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
 from functools import partial
-from typing import Any, Callable, Tuple
-from jax.typing import ArrayLike
+from typing import Callable, Tuple
 
 import jax.numpy as jnp
+from jax.typing import ArrayLike
 
 from flax import nnx
 from flax.core import Scope  # noqa
@@ -38,17 +38,18 @@ class DnCNNNet(nnx.Module):
     Flax nnx implementation of the convolutional neural network (CNN)
     architecture for denoising described in :cite:`zhang-2017-dncnn`.
     """
-    
-    def __init__(self,
-                 depth: int,
-                 channels: int,
-                 num_filters: int = 64,
-                 kernel_size: Tuple[int, int] = (3, 3),
-                 strides: Tuple[int, int] = (1, 1),
-                 act: Callable[..., ArrayLike] = nnx.relu,
-                 kernel_init: Callable = nnx.initializers.kaiming_normal,
-                 dtype=jnp.float32,
-                 rngs: nnx.Rngs = nnx.Rngs(0),
+
+    def __init__(
+        self,
+        depth: int,
+        channels: int,
+        num_filters: int = 64,
+        kernel_size: Tuple[int, int] = (3, 3),
+        strides: Tuple[int, int] = (1, 1),
+        act: Callable[..., ArrayLike] = nnx.relu,
+        kernel_init: Callable = nnx.initializers.kaiming_normal,
+        dtype=jnp.float32,
+        rngs: nnx.Rngs = nnx.Rngs(0),
     ):
         """Initialize DnCNN model.
 
@@ -63,20 +64,49 @@ class DnCNNNet(nnx.Module):
             kernel_init: Flax function for initializing the convolution kernels. Default:
                 :func:`~flax.nnx.initializers.kaiming_normal`.
             dtype: Output dtype. Default: :attr:`~numpy.float32`.
-            rngs: Random generation key.                
+            rngs: Random generation key.
         """
         super().__init__()
-        
+
         self.blocks = nnx.Sequential(
-            nnx.Conv(channels, num_filters, kernel_size=kernel_size, strides=strides, padding="CIRCULAR", use_bias=False, dtype=dtype, kernel_init=kernel_init(), rngs=rngs),
+            nnx.Conv(
+                channels,
+                num_filters,
+                kernel_size=kernel_size,
+                strides=strides,
+                padding="CIRCULAR",
+                use_bias=False,
+                dtype=dtype,
+                kernel_init=kernel_init(),
+                rngs=rngs,
+            ),
             act,
             *[
                 nnx.Sequential(
-                    ConvBNBlock(num_filters, num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs),
+                    ConvBNBlock(
+                        num_filters,
+                        num_filters,
+                        act,
+                        kernel_size,
+                        strides,
+                        kernel_init,
+                        dtype,
+                        rngs=rngs,
+                    ),
                 )
                 for _ in range(depth - 2)
             ],
-            nnx.Conv(num_filters, channels, kernel_size=kernel_size, strides=strides, padding="CIRCULAR", use_bias=False, dtype=dtype, kernel_init=kernel_init(), rngs=rngs),
+            nnx.Conv(
+                num_filters,
+                channels,
+                kernel_size=kernel_size,
+                strides=strides,
+                padding="CIRCULAR",
+                use_bias=False,
+                dtype=dtype,
+                kernel_init=kernel_init(),
+                rngs=rngs,
+            ),
         )
 
     def __call__(self, inputs: ArrayLike) -> ArrayLike:
@@ -101,16 +131,17 @@ class ResNet(nnx.Module):
     normalization blocks and ending with residual connection (i.e. adding
     the input to the output of the block)."""
 
-    def __init__(self,
-                 depth: int,
-                 channels: int,
-                 num_filters: int = 64,
-                 kernel_size: Tuple[int, int] = (3, 3),
-                 strides: Tuple[int, int] = (1, 1),
-                 act: Callable[..., ArrayLike] = nnx.relu,
-                 kernel_init: Callable = nnx.initializers.xavier_normal,
-                 dtype=jnp.float32,
-                 rngs: nnx.Rngs = nnx.Rngs(0),
+    def __init__(
+        self,
+        depth: int,
+        channels: int,
+        num_filters: int = 64,
+        kernel_size: Tuple[int, int] = (3, 3),
+        strides: Tuple[int, int] = (1, 1),
+        act: Callable[..., ArrayLike] = nnx.relu,
+        kernel_init: Callable = nnx.initializers.xavier_normal,
+        dtype=jnp.float32,
+        rngs: nnx.Rngs = nnx.Rngs(0),
     ):
         """Initialize ResNet model.
 
@@ -127,21 +158,40 @@ class ResNet(nnx.Module):
             kernel_init: Flax function for initializing the convolution kernels. Default:
                 :func:`~flax.nnx.initializers.xavier_normal`.
             dtype: Output dtype. Default: :attr:`~numpy.float32`.
-            rngs: Random generation key.                
+            rngs: Random generation key.
         """
         super().__init__()
-        
+
         self.blocks = nnx.Sequential(
-            ConvBNBlock(channels, num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs),
+            ConvBNBlock(
+                channels, num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs
+            ),
             *[
                 nnx.Sequential(
-                    ConvBNBlock(num_filters, num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs),
+                    ConvBNBlock(
+                        num_filters,
+                        num_filters,
+                        act,
+                        kernel_size,
+                        strides,
+                        kernel_init,
+                        dtype,
+                        rngs=rngs,
+                    ),
                 )
                 for _ in range(depth - 2)
             ],
-            ConvBNBlock(num_filters, channels, nnx.identity, kernel_size, strides, kernel_init, dtype, rngs=rngs),
+            ConvBNBlock(
+                num_filters,
+                channels,
+                nnx.identity,
+                kernel_size,
+                strides,
+                kernel_init,
+                dtype,
+                rngs=rngs,
+            ),
         )
-
 
     def __call__(self, x: ArrayLike) -> ArrayLike:
         """Apply ResNet.
@@ -163,16 +213,17 @@ class ConvBNNet(nnx.Module):
     Net constructed from sucessive applications of convolution plus batch
     normalization and activation blocks. No residual connection."""
 
-    def __init__(self,
-                 depth: int,
-                 channels: int,
-                 num_filters: int = 64,
-                 kernel_size: Tuple[int, int] = (3, 3),
-                 strides: Tuple[int, int] = (1, 1),
-                 act: Callable[..., ArrayLike] = nnx.relu,
-                 kernel_init: Callable = nnx.initializers.xavier_normal,
-                 dtype=jnp.float32,
-                 rngs: nnx.Rngs = nnx.Rngs(0),
+    def __init__(
+        self,
+        depth: int,
+        channels: int,
+        num_filters: int = 64,
+        kernel_size: Tuple[int, int] = (3, 3),
+        strides: Tuple[int, int] = (1, 1),
+        act: Callable[..., ArrayLike] = nnx.relu,
+        kernel_init: Callable = nnx.initializers.xavier_normal,
+        dtype=jnp.float32,
+        rngs: nnx.Rngs = nnx.Rngs(0),
     ):
         """Initialize ConvBNNet model.
 
@@ -189,19 +240,39 @@ class ConvBNNet(nnx.Module):
             kernel_init: Flax function for initializing the convolution kernels. Default:
                 :func:`~flax.nnx.initializers.xavier_normal`.
             dtype: Output dtype. Default: :attr:`~numpy.float32`.
-            rngs: Random generation key.                
+            rngs: Random generation key.
         """
         super().__init__()
-        
+
         self.blocks = nnx.Sequential(
-            ConvBNBlock(channels, num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs),
+            ConvBNBlock(
+                channels, num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs
+            ),
             *[
                 nnx.Sequential(
-                    ConvBNBlock(num_filters, num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs),
+                    ConvBNBlock(
+                        num_filters,
+                        num_filters,
+                        act,
+                        kernel_size,
+                        strides,
+                        kernel_init,
+                        dtype,
+                        rngs=rngs,
+                    ),
                 )
                 for _ in range(depth - 2)
             ],
-            ConvBNBlock(num_filters, channels, nnx.identity, kernel_size, strides, kernel_init, dtype, rngs=rngs),
+            ConvBNBlock(
+                num_filters,
+                channels,
+                nnx.identity,
+                kernel_size,
+                strides,
+                kernel_init,
+                dtype,
+                rngs=rngs,
+            ),
         )
 
     def __call__(self, x: ArrayLike) -> ArrayLike:
@@ -218,20 +289,21 @@ class ConvBNNet(nnx.Module):
 
 class UNet(nnx.Module):
     """Flax nnx implementation of U-Net model :cite:`ronneberger-2015-unet`."""
-    
-    def __init__(self,
-                 depth: int,
-                 channels: int,
-                 num_filters: int = 64,
-                 kernel_size: Tuple[int, int] = (3, 3),
-                 strides: Tuple[int, int] = (1, 1),
-                 block_depth: int = 2,
-                 window_shape: Tuple[int, int] = (2, 2),
-                 upsampling: int = 2,
-                 act: Callable[..., ArrayLike] = nnx.relu,
-                 kernel_init: Callable = nnx.initializers.kaiming_normal,
-                 dtype=jnp.float32,
-                 rngs: nnx.Rngs = nnx.Rngs(0),
+
+    def __init__(
+        self,
+        depth: int,
+        channels: int,
+        num_filters: int = 64,
+        kernel_size: Tuple[int, int] = (3, 3),
+        strides: Tuple[int, int] = (1, 1),
+        block_depth: int = 2,
+        window_shape: Tuple[int, int] = (2, 2),
+        upsampling: int = 2,
+        act: Callable[..., ArrayLike] = nnx.relu,
+        kernel_init: Callable = nnx.initializers.kaiming_normal,
+        dtype=jnp.float32,
+        rngs: nnx.Rngs = nnx.Rngs(0),
     ):
         """Initialize U-Net model.
 
@@ -251,41 +323,104 @@ class UNet(nnx.Module):
             kernel_init: Flax function for initializing the convolution kernels. Default:
                 :func:`~flax.nnx.initializers.kaiming_normal`.
             dtype: Output dtype. Default: :attr:`~numpy.float32`.
-            rngs: Random generation key.                
+            rngs: Random generation key.
         """
         super().__init__()
-        
+
         # Processing at input resolution
-        self.start = ConvBNMultiBlock(channels, block_depth, num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs)
-  
+        self.start = ConvBNMultiBlock(
+            channels,
+            block_depth,
+            num_filters,
+            act,
+            kernel_size,
+            strides,
+            kernel_init,
+            dtype,
+            rngs=rngs,
+        )
+
         # Downsampling path
         self.down = nnx.List([])
         for j in range(depth - 1):
-            self.down.append(nnx.Sequential(
-                *[
-                    ConvBNPoolBlock(2**j * num_filters, 2 * 2**j * num_filters, act, nnx.max_pool, kernel_size, strides, window_shape, kernel_init, dtype, rngs=rngs),
-                    ConvBNMultiBlock(2 * 2**j * num_filters, block_depth, 2 * 2**j * num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs)
-                ])
+            self.down.append(
+                nnx.Sequential(
+                    *[
+                        ConvBNPoolBlock(
+                            2**j * num_filters,
+                            2 * 2**j * num_filters,
+                            act,
+                            nnx.max_pool,
+                            kernel_size,
+                            strides,
+                            window_shape,
+                            kernel_init,
+                            dtype,
+                            rngs=rngs,
+                        ),
+                        ConvBNMultiBlock(
+                            2 * 2**j * num_filters,
+                            block_depth,
+                            2 * 2**j * num_filters,
+                            act,
+                            kernel_size,
+                            strides,
+                            kernel_init,
+                            dtype,
+                            rngs=rngs,
+                        ),
+                    ]
+                )
             )
-        
+
         # Definition of upscaling function
         upfn = partial(upscale_nn, scale=upsampling)
 
         # Upsampling path
         self.up = nnx.List([])
         for j in reversed(range(depth - 1)):
-            self.up.append(nnx.List(
-                [
-                    ConvBNUpsampleBlock(2 * 2**j * num_filters, 2**j * num_filters, act, upfn, kernel_size, strides, kernel_init, dtype, rngs=rngs),
-                    ConvBNMultiBlock(2 * 2**j * num_filters, block_depth, 2**j * num_filters, act, kernel_size, strides, kernel_init, dtype, rngs=rngs)
-                ])
+            self.up.append(
+                nnx.List(
+                    [
+                        ConvBNUpsampleBlock(
+                            2 * 2**j * num_filters,
+                            2**j * num_filters,
+                            act,
+                            upfn,
+                            kernel_size,
+                            strides,
+                            kernel_init,
+                            dtype,
+                            rngs=rngs,
+                        ),
+                        ConvBNMultiBlock(
+                            2 * 2**j * num_filters,
+                            block_depth,
+                            2**j * num_filters,
+                            act,
+                            kernel_size,
+                            strides,
+                            kernel_init,
+                            dtype,
+                            rngs=rngs,
+                        ),
+                    ]
+                )
             )
-        
+
         # Final conv1x1
         ksz_out = (1, 1)
-        self.end = nnx.Conv(num_filters, channels, kernel_size=ksz_out, strides=strides, padding="CIRCULAR", use_bias=False, dtype=dtype, kernel_init=kernel_init(), rngs=rngs
+        self.end = nnx.Conv(
+            num_filters,
+            channels,
+            kernel_size=ksz_out,
+            strides=strides,
+            padding="CIRCULAR",
+            use_bias=False,
+            dtype=dtype,
+            kernel_init=kernel_init(),
+            rngs=rngs,
         )
-
 
     def __call__(self, x: ArrayLike) -> ArrayLike:
         """Apply U-Net.
@@ -297,20 +432,18 @@ class UNet(nnx.Module):
             The U-Net result.
         """
         out = self.start(x)
-        
+
         # going down
         residual = []
         for block in self.down:
             residual.append(out)  # for skip connections
-            out = block(out) # Pooling and multiconvbn block
+            out = block(out)  # Pooling and multiconvbn block
 
         # going up
-        res_ind = -1
         for block in self.up:
-            out = block[0](out) # Upsample block
+            out = block[0](out)  # Upsample block
             # skip connection
-            out = jnp.concatenate((residual[res_ind], out), axis=-1) # channel last
+            out = jnp.concatenate((residual.pop(), out), axis=-1)  # channel last
             out = block[1](out)
-            res_ind -= 1
-        
+
         return self.end(out)
