e03a2d039404d645097e72625177cfcaf18f213c
Brendt Wohlberg <brendt@ieee.org>
2026-05-07 18:11:47 -0600

    Clean up import, __all_, and docs append mechanisms

--- scico/_core.py
+++ scico/_core.py
@@ -7,6 +7,7 @@
 """Extensions of core jax functions, including tools for automatic differentiation
 and shape evaluation."""
 
+import sys
 from typing import Any, Callable, Optional, Sequence, Tuple, Union
 
 import jax
@@ -15,6 +16,16 @@ from jax.tree_util import tree_map
 import scico.numpy
 import scico.numpy.util
 import scico.util
+
+__all__ = [
+    "cvjp",
+    "eval_shape",
+    "grad",
+    "jacrev",
+    "linear_adjoint",
+    "linear_transpose",
+    "value_and_grad",
+]
 
 
 def _append_jax_docs(fn, jaxfn=None):
@@ -51,24 +62,10 @@ def eval_shape(fun: Callable, *args, **kwargs) -> Any:
     a call to :func:`jax.eval_shape`, with ``args`` and ``kwargs`` mapped
     to handle :class:`jax.ShapeDtypeStruct` objects with nested shapes
     corresponding to :class:`.BlockArray` objects.
-
-    Args:
-        fun: The function for which the output shape/dtype are to be
-          evaluated.
-        *args: Positional arguments.
-        **kwargs: Keyword Arguments.
-
-    Returns:
-        A nested PyTree containing :class:`jax.ShapeDtypeStruct` objects
-        as leaves.
     """
     mapped_args = jax.tree_util.tree_map(_convert_ba_dts, args)
     mapped_kwargs = jax.tree_util.tree_map(_convert_ba_dts, kwargs)
     return jax.eval_shape(fun, *mapped_args, **mapped_kwargs)
-
-
-# Append docstring from original jax function
-eval_shape.__doc__ = _append_jax_docs(eval_shape)
 
 
 def grad(
@@ -97,10 +94,6 @@ def grad(
         return tree_map(jax.numpy.conj, jg)
 
     return conjugated_grad_aux if has_aux else conjugated_grad
-
-
-# Append docstring from original jax function
-grad.__doc__ = _append_jax_docs(grad)
 
 
 def value_and_grad(
@@ -132,10 +125,6 @@ def value_and_grad(
     return conjugated_value_and_grad_aux if has_aux else conjugated_value_and_grad
 
 
-# Append docstring from original jax function
-value_and_grad.__doc__ = _append_jax_docs(value_and_grad)
-
-
 def linear_transpose(fun: Callable, *primals) -> Callable:
     """Transpose a function that is guaranteed to be linear.
 
@@ -146,10 +135,6 @@ def linear_transpose(fun: Callable, *primals) -> Callable:
     """
     mapped_primals = jax.tree_util.tree_map(_convert_ba_dts, primals)
     return jax.linear_transpose(fun, *mapped_primals)
-
-
-# Append docstring from original jax function
-linear_transpose.__doc__ = _append_jax_docs(linear_transpose, jaxfn=jax.linear_transpose)
 
 
 def linear_adjoint(fun: Callable, *primals) -> Callable:
@@ -166,10 +151,6 @@ def linear_adjoint(fun: Callable, *primals) -> Callable:
         return tree_map(jax.numpy.conj, fun(*(conj_primals)))
 
     return linear_transpose(conj_fun, *primals)
-
-
-# Append docstring from original jax function
-linear_adjoint.__doc__ = _append_jax_docs(linear_adjoint, jaxfn=jax.linear_transpose)
 
 
 def jacrev(
@@ -191,10 +172,6 @@ def jacrev(
         return tree_map(jax.numpy.conj, tmp)
 
     return conjugated_jacrev
-
-
-# Append docstring from original jax function
-jacrev.__doc__ = _append_jax_docs(jacrev)
 
 
 def cvjp(fun: Callable, *primals, jidx: Optional[int] = None) -> Tuple[Tuple[Any, ...], Callable]:
@@ -238,3 +215,12 @@ def cvjp(fun: Callable, *primals, jidx: Optional[int] = None) -> Tuple[Tuple[Any
         return jax.tree_util.tree_map(jax.numpy.conj, fun_vjp(tangent.conj()))
 
     return primals_out, conj_vjp
+
+
+# Append docstring from original jax function
+for name in __all__:
+    if name == "cvjp":
+        continue
+    func = getattr(sys.modules[__name__], name)
+    jaxfn = jax.linear_transpose if name == "linear_adjoint" else None
+    func.__doc__ = _append_jax_docs(func, jaxfn=jaxfn)
