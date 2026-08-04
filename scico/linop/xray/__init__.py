bfc6f58887e2afb1963650f5a3e66cab71fa97de
Brendt Wohlberg <brendt@ieee.org>
2026-08-04 17:39:51 -0600

    Split _xray module into separate 2d and 3d modules

--- scico/linop/xray/__init__.py
+++ scico/linop/xray/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2023-2025 by SCICO Developers
+# Copyright (C) 2023-2026 by SCICO Developers
 # All rights reserved. BSD 3-clause License.
 # This file is part of the SCICO package. Details of the copyright and
 # user license can be found in the 'LICENSE' file distributed with the
@@ -73,7 +73,8 @@ from ._util import (
     rotate_volume,
     volume_alignment_rotation,
 )
-from ._xray import XRayTransform2D, XRayTransform3D
+from ._xray2d import XRayTransform2D
+from ._xray3d import XRayTransform3D
 
 __all__ = [
     "XRayTransform2D",
